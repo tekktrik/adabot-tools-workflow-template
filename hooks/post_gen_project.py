cf57bf1afa38980630de5262c43bc23894e90238
Alec Delaney <tekktrik@gmail.com>
2022-06-30 19:46:37 -0400

    Remove debugging statements

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,10 +2,8 @@
 #
 # SPDX-License-Identifier: MIT
 
-import os
 import subprocess
 
 subprocess.Popen(["git", "init"]).communicate()
 subprocess.Popen(["git", "submodule", "add", "https://github.com/adafruit/adabot.git", "submodules/adabot"]).communicate()
 subprocess.Popen(["git", "submodule", "update", "--init", "submodules/adabot"]).communicate()
-print(os.getcwd())
